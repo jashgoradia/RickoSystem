aa044d3cde19a5a7d425316fa03717d61e2e189a
goradia j.j. (jg1u17) <jg1u17@soton.ac.uk>
2020-03-06 20:08:11 +0000

    Working on similarity matrix

--- src/main/java/SimpleDB2.java
+++ src/main/java/SimpleDB2.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.sql.*;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map.Entry;
 
 /**
@@ -21,11 +23,12 @@ public class SimpleDB2 {
     final String url = "jdbc:sqlite:" + cwd + "/sqlite/db/comp3208.db";
     final String trainingset_tablename = "training_dataset";
     public Connection conn;
-
+    //private int nItems = itemCount();
     /**
      * The data is stored in a HashMap, which allows fast access.
      */
-    public HashMap<Integer, HashMap<Integer, HashMap<Float,Integer>>>data;
+    private HashMap<Integer, HashMap<Integer,HashMap<Float,Integer>>>itemBased;
+    //private HashMap<Integer, Float>avgRating;
 
     /**
      * Open an existing database.
@@ -62,7 +65,7 @@ public class SimpleDB2 {
             //store results of query
 
 
-            data = new HashMap<>();
+            itemBased = new HashMap<>();
 
             //loop through result set
             while (rs.next()) {
@@ -72,12 +75,12 @@ public class SimpleDB2 {
                 Float rating = rs.getFloat(3);
                 Integer time_stamp = rs.getInt(4);
 
-                HashMap<Integer, HashMap<Float,Integer>> itemRatings = data.get(item);
+                HashMap<Integer, HashMap<Float,Integer>> itemRatings = itemBased.get(item);
                 // check if this iem already exists. If not, create a new
                 // HashMap for this item.
                 if (itemRatings == null) {
                     itemRatings = new HashMap<>();
-                    data.put(item, itemRatings);
+                    itemBased.put(item, itemRatings);
                 }
                 HashMap<Float,Integer> userRatings = itemRatings.get(user);
                 userRatings = new HashMap<>();
@@ -90,14 +93,59 @@ public class SimpleDB2 {
                 // don't forget to dispose any prepared statements
                 stat.dispose();
             */
-            System.out.println("Loaded " + count + " ratings from " + data.size() + " items.");
+            System.out.println("Loaded " + count + " ratings from " + itemBased.size() + " items.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    public int itemCount() {
+        String sql = "SELECT COUNT(DISTINCT item_id) FROM training_dataset";
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+           return (rs.getInt(1));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public void simMatrix(){
+        int[][] similarityMatrix = new int [itemCount()][itemCount()];
+
+
+    }
+
+    public float cosineSimilarity (int item1, int item2){
+        HashSet<Integer> intersection = new HashSet<Integer>(itemBased.get(item1).keySet());
+        intersection.retainAll(itemBased.get(item2).keySet());
+        Iterator<Integer> it = intersection.iterator();
+        while(it.hasNext()){
+            float avg_rating = avgRating(it.next());
+
+        }
+
+        return 0;
+    }
+
+    public float avgRating(Integer userid){
+        String sql = "SELECT AVG(ratings) FROM training_dataset WHERE user_id = userid";
+        try(Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+
+            return (rs.getFloat(1));
+
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+            return 0.0f;
+        }
+    }
+
     public static void main (String[] args) {
         SimpleDB2 sdb = new SimpleDB2();
-        sdb.loadRatings();
+        System.out.println(sdb.avgRating(2943));
+        //sdb.loadRatings();
+        //System.out.println(sdb.itemCount());
+
     }
 }
