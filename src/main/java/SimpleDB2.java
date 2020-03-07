891cff6861fe14bc96f0cb7b54954b4489ac0f36
goradia j.j. (jg1u17) <jg1u17@soton.ac.uk>
2020-03-07 19:27:06 +0000

    Coeff calculation seems complete, issue with caluclating average rating

--- src/main/java/SimpleDB2.java
+++ src/main/java/SimpleDB2.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.sql.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 import java.util.Map.Entry;
 
 /**
@@ -27,7 +25,8 @@ public class SimpleDB2 {
     /**
      * The data is stored in a HashMap, which allows fast access.
      */
-    private HashMap<Integer, HashMap<Integer,HashMap<Float,Integer>>>itemBased;
+    private HashMap<Integer, HashMap<Integer,Float>>itemBased;
+    private HashMap<Integer,Float>avgRating;
     //private HashMap<Integer, Float>avgRating;
 
     /**
@@ -52,7 +51,7 @@ public class SimpleDB2 {
     public void loadRatings() {
         System.out.println("Loading data from table " + trainingset_tablename);
         int count = 0;
-        String sql = "SELECT user_id, item_id, rating,time_stamp FROM "+trainingset_tablename;
+        String sql = "SELECT user_id, item_id, rating FROM "+trainingset_tablename;
         try(Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql)){
 
@@ -73,20 +72,31 @@ public class SimpleDB2 {
                 Integer user = rs.getInt(1);
                 Integer item = rs.getInt(2);
                 Float rating = rs.getFloat(3);
-                Integer time_stamp = rs.getInt(4);
+                //Integer time_stamp = rs.getInt(4);
 
-                HashMap<Integer, HashMap<Float,Integer>> itemRatings = itemBased.get(item);
+                HashMap<Integer, Float> itemRatings = itemBased.get(item);
                 // check if this iem already exists. If not, create a new
                 // HashMap for this item.
                 if (itemRatings == null) {
                     itemRatings = new HashMap<>();
                     itemBased.put(item, itemRatings);
                 }
-                HashMap<Float,Integer> userRatings = itemRatings.get(user);
-                userRatings = new HashMap<>();
-                userRatings.put(rating,time_stamp);
-                itemRatings.put(user, userRatings);
+                //HashMap<Float,Integer> userRatings = itemRatings.get(user);
+                //userRatings = new HashMap<>();
+                //userRatings.put(rating,time_stamp);
+                itemRatings.put(user, rating);
                 count++;
+
+                //if(avgRating.get(user).equals(null)){
+                    String sql1 = "SELECT AVG(rating) FROM training_dataset WHERE user_id= "+ user;
+                    Statement stmt1 = conn.createStatement();
+                    ResultSet rs1 = stmt1.executeQuery(sql1);
+
+                    if(rs1 != null)
+                        avgRating.put(user,rs1.getFloat(1));
+                    else
+                        avgRating.put(user,0.0f);
+                //}
 
             }
             /* Don't think this is needed
@@ -99,7 +109,7 @@ public class SimpleDB2 {
         }
     }
 
-    public int itemCount() {
+    /*public int itemCount() {
         String sql = "SELECT COUNT(DISTINCT item_id) FROM training_dataset";
         try (Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
@@ -108,42 +118,56 @@ public class SimpleDB2 {
             System.out.println(e.getMessage());
             return 0;
         }
-    }
+    }*/
 
     public void simMatrix(){
-        int[][] similarityMatrix = new int [itemCount()][itemCount()];
-
+        List<Integer> items = new ArrayList<>(itemBased.keySet());
+        for (int i = 0; i<items.size(); i++){
+            for (int j = i+1; j<items.size(); j++){
+                System.out.println(cosineSimilarity(items.get(i),items.get(j)));
+            }
+            System.out.println();
+        }
 
     }
 
     public float cosineSimilarity (int item1, int item2){
+
+        float numerator = 0.0f;
+        float denominator_left = 0.0f;
+        float denominator_right = 0.0f;
+
         HashSet<Integer> intersection = new HashSet<Integer>(itemBased.get(item1).keySet());
         intersection.retainAll(itemBased.get(item2).keySet());
-        Iterator<Integer> it = intersection.iterator();
-        while(it.hasNext()){
-            float avg_rating = avgRating(it.next());
 
+        if(intersection!=null) {
+
+            for (int user : intersection) {
+                //ratings.put(itemBased.get(item1).get(user),itemBased.get(item2).get(user));
+                float avg_rating = avgRating.get(user);
+                float item1_rating = itemBased.get(item1).get(user);
+                float item2_rating = itemBased.get(item2).get(user);
+
+                numerator += ((item1_rating - avg_rating) * (item2_rating - avg_rating));
+                denominator_left += (float) Math.pow((item1_rating - avg_rating), 2);
+                denominator_right += (float) Math.pow((item2_rating - avg_rating), 2);
+            }
+            denominator_left = (float) Math.sqrt(denominator_left);
+            denominator_right = (float) Math.sqrt(denominator_right);
+
+            return (numerator / (denominator_left * denominator_right));
         }
 
-        return 0;
-    }
-
-    public float avgRating(Integer userid){
-        String sql = "SELECT AVG(ratings) FROM training_dataset WHERE user_id = userid";
-        try(Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql)){
-
-            return (rs.getFloat(1));
-
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
+        else {
             return 0.0f;
         }
     }
 
     public static void main (String[] args) {
         SimpleDB2 sdb = new SimpleDB2();
-        System.out.println(sdb.avgRating(2943));
+        sdb.loadRatings();
+        //sdb.simMatrix();
+        //System.out.println(sdb.avgRating(2943));
         //sdb.loadRatings();
         //System.out.println(sdb.itemCount());
 
