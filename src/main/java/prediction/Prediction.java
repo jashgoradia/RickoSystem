01f23f204cfb8366837fa2e7dfce4089b78a32b3
jashgoradia <goradiajash@gmail.com>
2020-04-10 01:05:07 +0100

    Prediction function seems to be working fine, could add threshold or neighbours value as parameters later on

--- src/main/java/prediction/Prediction.java
+++ src/main/java/prediction/Prediction.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
+import java.time.LocalTime;
 import java.util.*;
 
 public class Prediction {
@@ -14,9 +15,9 @@ public class Prediction {
     String url;
     String training_table;
     Map<Integer,List<Integer>> test_set = new TreeMap<>();
-    LinkedHashMap<Integer,HashMap<Integer,Float>> data = new LinkedHashMap<>();
-    HashMap<Integer,Float> avgItemRating = new HashMap<>();
-    HashMap<Integer,Float> avgUserRating = new HashMap<>();
+    Map<Integer,TreeMap<Integer,Float>> data = new TreeMap<>();
+    TreeMap<Integer,Float> avgItemRating = new TreeMap<>();
+    TreeMap<Integer,Float> avgUserRating = new TreeMap<>();
     Set<Integer> items = new HashSet<>();
     public Prediction(String url,String tableName,String training_table){
         this.tableName=tableName;
@@ -57,8 +58,7 @@ public class Prediction {
 
     public void loadTrainData(){
         System.out.println("Loading data from table "+ training_table);
-        //FIXME not sure whether I need all users and items from training set or just those that are also in test set
-        String sql = "SELECT user_id,item_id,rating FROM " + training_table;
+        String sql = "SELECT user_id,item_id,rating FROM " + training_table+ " WHERE user_id in (SELECT DISTINCT user_id FROM "+tableName+")";
 
         Connect c = new Connect();
         int count=0;
@@ -73,39 +73,60 @@ public class Prediction {
                 int item_id = rs.getInt(2);
                 float rating = rs.getFloat(3);
 
-                HashMap<Integer,Float> userRatings = data.get(user_id);
+                TreeMap<Integer,Float> userRatings = data.get(user_id);
 
                 if(userRatings == null){
-                    userRatings = new HashMap<>();
+                    userRatings = new TreeMap<>();
                     data.put(user_id,userRatings);
                 }
 
                 userRatings.put(item_id,rating);
-                items.add(item_id);
                 count++;
             }
             conn.close();
             System.out.println("Loaded "+count+" ratings from " + data.size() + " users.");
-            System.out.println("Loaded "+items.size()+" items from "+training_table);
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
 
+    public void itemList(){
+        System.out.println("Storing list of all items that are in both sets");
+        String sql = "SELECT DISTINCT item_id FROM "+training_table+" WHERE item_id IN (SELECT DISTINCT item_id from "+tableName+")";
+        Connect c = new Connect();
+        int count=0;
+        try(Connection conn = c.connect(url);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+        ){
+            while (rs.next()) {
+                items.add(rs.getInt(1));
+                count++;
+            }
+            conn.close();
+            stmt.close();
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Loaded "+count+" items from training set");
+    }
+
     public void loadAvgUserRating(){
         System.out.println("Calculating average user rating");
-        String sql = "SELECT user_id, AVG(rating) FROM "+training_table+" GROUP BY user_id";
+        String sql = "SELECT user_id, AVG(rating) FROM "+training_table+" WHERE user_id in (SELECT DISTINCT user_id FROM "+tableName+") GROUP BY user_id";
         Connect c = new Connect();
+        int count = 0;
         try(Connection conn = c.connect(url);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql)){
-            int count = 0;
+            int flag = 0;
             while(rs.next()){
                 try {
                     avgUserRating.put(rs.getInt(1), rs.getFloat(2));
-                } catch (NullPointerException e){
                     count++;
-                    System.out.println(count);
+                } catch (NullPointerException e){
+                    flag++;
+                    System.out.println(flag);
                 }
             }
             conn.close();
@@ -113,23 +134,24 @@ public class Prediction {
         } catch (SQLException e){
             System.out.println(e.getMessage());
         }
-        System.out.println("Average user rating done");
+        System.out.println("Loaded average ratings for "+count+" users.");
     }
 
     public void loadAvgItemRating(){
         System.out.println("Calculating average item rating");
-        String sql = "SELECT item_id, AVG(rating) FROM "+training_table+" GROUP BY item_id";
+        String sql = "SELECT item_id, AVG(rating) FROM "+training_table+" WHERE item_id IN (SELECT DISTINCT item_id FROM "+tableName+") GROUP BY item_id";
         Connect c = new Connect();
+        int count=0;
         try(Connection conn = c.connect(url);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql)){
-            int count = 0;
+            int flag = 0;
             while(rs.next()){
                 try {
                     avgItemRating.put(rs.getInt(1), rs.getFloat(2));
                 } catch (NullPointerException e){
-                    count++;
-                    System.out.println(count);
+                    flag++;
+                    System.out.println(flag);
                 }
             }
             conn.close();
@@ -137,7 +159,7 @@ public class Prediction {
         } catch (SQLException e){
             System.out.println(e.getMessage());
         }
-        System.out.println("Average item rating done");
+        System.out.println("Loaded average ratings for "+count+" items.");
     }
 
     public void predictionCalc(){
@@ -151,31 +173,33 @@ public class Prediction {
             items_count++;
             for(int user:test_set.get(item)){
                 float prediction = 0f;
-                //FIXME not sure if these checks are all valid
                 if((items.contains(item)==false && data.containsKey(user)==false) || neighbourhood==null){
                     Random r = new Random();
-                    prediction = (r.nextInt(10)+1)/2;
+                    prediction = toNearestPoint5((r.nextInt(10)+1)/2);
                 }
                 else if(items.contains(item)==false){
-                    prediction = avgUserRating.get(user);
+                    prediction = toNearestPoint5(avgUserRating.get(user));
                 }
                 else if(data.containsKey(user)==false){
-                    prediction = avgItemRating.get(item);
+                    prediction = toNearestPoint5(avgItemRating.get(item));
                 }
                 else {
                     Set<Integer> neighbours = data.get(user).keySet();
                     row_count++;
+                    float numerator =0f;
+                    float denominator = 0f;
                     //TODO could add neighbour lower and upper limit
                     for (int neighbour : neighbours) {
-                        //TODO separate the prediction calc to a separate method
-                        if (neighbourhood.containsKey(neighbour)) {
+                        if (neighbourhood.containsKey(neighbour)) { //TODO separate the prediction calc to a separate method
                             float rating = data.get(user).get(neighbour);
                             float sim = neighbourhood.get(neighbour);
-                            prediction += ((sim * rating) / sim); //FIXME this doesn't work because numerator and denominator need to be summationed separately
+                            numerator+=(sim*rating);
+                            denominator+=(sim);
                         }
                     }
+                    prediction=numerator/denominator;
                 }
-                preds.add(new String[]{String.valueOf(user), String.valueOf(item), String.valueOf(prediction)});
+                preds.add(new String[]{String.valueOf(user), String.valueOf(item), String.valueOf(toNearestPoint5(prediction))});
                 if (preds.size() >= 100000) { //TODO not sure if 100,000 writes are good
                     writeToCsv(preds);
                     preds.clear();
@@ -183,6 +207,14 @@ public class Prediction {
                 System.out.println("Items: "+ items_count+ ", Row: "+row_count);
             }
         }
+    }
+
+    public float toNearestPoint5(float value){
+        float newValue = (float) (0.5*Math.round(value*2));
+        if(newValue==0){
+            newValue=0.5f;
+        }
+        return newValue;
     }
 
 
@@ -203,8 +235,7 @@ public class Prediction {
     }
 
     public HashMap<Integer,Float> getNeighbourhood (int item){
-        //TODO could add threshold as a parameter
-        String sql= "SELECT item2,sim FROM sim_matrix where item1 = ? and sim>0";
+        String sql= "SELECT item2,sim FROM sim_matrix where item1 = ? and sim>0"; //TODO could add threshold as a parameter
 
         Connect c = new Connect();
         HashMap<Integer,Float> result = new HashMap<>();
@@ -232,11 +263,15 @@ public class Prediction {
         String cwd = new File("").getAbsolutePath();
         String url = "jdbc:sqlite:" + cwd + "/sqlite/db/comp3208.db";
         Prediction p = new Prediction(url,"testing_dataset","training_dataset");
-
+        System.out.println(LocalTime.now());
         p.loadData();
         p.loadTrainData();
+        p.itemList();
         p.loadAvgUserRating();
         p.loadAvgItemRating();
+        System.out.println(LocalTime.now());
         p.predictionCalc();
+        System.out.println(LocalTime.now());
+        //
     }
 }
